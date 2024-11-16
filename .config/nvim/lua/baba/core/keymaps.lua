7ae25d6a4f9090f08407ff91362f01e27f6b98c7
Urvish <urvishp45003@gmail.com>
2024-11-16 23:19:54 +0530

    removed bufferline and harpoon

--- .config/nvim/lua/baba/core/keymaps.lua
+++ .config/nvim/lua/baba/core/keymaps.lua
@@ -35,16 +35,60 @@ keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) 
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 -- Navigate to next/previous buffer
-keymap.set("n", "<C-i>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
-keymap.set("n", "<C-o>", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
+keymap.set("n", "<C-o>", "<cmd>bnext<CR>", { desc = "Go to next buffer" })
+keymap.set("n", "<C-i>", "<cmd>bprevious<CR>", { desc = "Go to previous buffer" })
 
 -- Close current buffer
 keymap.set("n", "<leader>bx", "<cmd>bdelete<CR>", { desc = "Close current buffer" })
 keymap.set("n", "<leader>bf", "<cmd>BufferLinePick<CR>", { desc = "Go to buffer (fuzzy)" })
+
+-- Undotree
+keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+--------------------- Barbar Keymaps ---------------------
+-- Move to previous/next
+keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { desc = "Go to previous buffer" })
+keymap.set("n", "<A-.>", "<Cmd>BufferNext<CR>", { desc = "Go to next buffer" })
+
+-- Re-order to previous/next
+keymap.set("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", { desc = "Move buffer to previous position" })
+keymap.set("n", "<A->>", "<Cmd>BufferMoveNext<CR>", { desc = "Move buffer to next position" })
+
+-- Goto buffer in position...
+keymap.set("n", "<A-1>", "<Cmd>BufferGoto 1<CR>", { desc = "Go to buffer 1" })
+keymap.set("n", "<A-2>", "<Cmd>BufferGoto 2<CR>", { desc = "Go to buffer 2" })
+keymap.set("n", "<A-3>", "<Cmd>BufferGoto 3<CR>", { desc = "Go to buffer 3" })
+keymap.set("n", "<A-4>", "<Cmd>BufferGoto 4<CR>", { desc = "Go to buffer 4" })
+keymap.set("n", "<A-5>", "<Cmd>BufferGoto 5<CR>", { desc = "Go to buffer 5" })
+keymap.set("n", "<A-6>", "<Cmd>BufferGoto 6<CR>", { desc = "Go to buffer 6" })
+keymap.set("n", "<A-7>", "<Cmd>BufferGoto 7<CR>", { desc = "Go to buffer 7" })
+keymap.set("n", "<A-8>", "<Cmd>BufferGoto 8<CR>", { desc = "Go to buffer 8" })
+keymap.set("n", "<A-9>", "<Cmd>BufferGoto 9<CR>", { desc = "Go to buffer 9" })
+keymap.set("n", "<A-0>", "<Cmd>BufferLast<CR>", { desc = "Go to last buffer" })
+
+-- Pin/unpin buffer
+keymap.set("n", "<A-p>", "<Cmd>BufferPin<CR>", { desc = "Pin/unpin buffer" })
+
+-- Close buffer
+keymap.set("n", "<A-c>", "<Cmd>BufferClose<CR>", { desc = "Close buffer" })
+
+-- Restore buffer
+keymap.set("n", "<A-S-c>", "<Cmd>BufferRestore<CR>", { desc = "Restore closed buffer" })
+
+-- Magic buffer-picking mode
+keymap.set("n", "<C-p>", "<Cmd>BufferPick<CR>", { desc = "Pick buffer" })
+keymap.set("n", "<C-p>", "<Cmd>BufferPickDelete<CR>", { desc = "Pick buffer to delete" })
+
+-- Sort automatically by...
+keymap.set("n", "<leader>bb", "<Cmd>BufferOrderByBufferNumber<CR>", { desc = "Sort buffers by number" })
+keymap.set("n", "<leader>bd", "<Cmd>BufferOrderByDirectory<CR>", { desc = "Sort buffers by directory" })
+keymap.set("n", "<leader>bl", "<Cmd>BufferOrderByLanguage<CR>", { desc = "Sort buffers by language" })
+keymap.set("n", "<leader>bn", "<Cmd>BufferOrderByName<CR>", { desc = "Sort buffers by name" })
+keymap.set("n", "<leader>bw", "<Cmd>BufferOrderByWindowNumber<CR>", { desc = "Sort buffers by window number" })
--- .config/nvim/lua/baba/plugins/colorscheme.lua
+++ .config/nvim/lua/baba/plugins/colorscheme.lua
@@ -1,41 +1,56 @@
 return {
+
 	{
-		"rose-pine/neovim",
-		name = "rose-pine",
+		"olivercederborg/poimandres.nvim",
+		lazy = false,
+		priority = 1000,
 		config = function()
-			require("rose-pine").setup({
-				variant = "main", -- auto, main, moon, or dawn
-				dark_variant = "main", -- main, moon, or dawn
-				-- dim_inactive_windows = false,
-				-- extend_background_behind_borders = true,
-				-- enable = {
-				-- 	terminal = true,
-				-- 	legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-				-- 	migrations = true, -- Handle deprecated options automatically
-				-- },
-				palette = {
-					main = {
-						base = "#000000",
-						surface = "#000000",
-						overlay = "#191724",
-					},
-				},
-			})
+			require("poimandres").setup({})
+		end,
 
-			vim.cmd("colorscheme rose-pine")
-			local custom_auto = require("lualine.themes.auto")
-			custom_auto.normal.c.bg = "#000000"
-
-			require("lualine").setup({
-				options = {
-					icons_enabled = false,
-					theme = custom_auto,
-					component_separators = "|",
-					section_separators = "",
-				},
-			})
+		-- optionally set the colorscheme within lazy config
+		init = function()
+			vim.cmd("colorscheme poimandres")
 		end,
 	},
+
+	-- {
+	-- 	"rose-pine/neovim",
+	-- 	name = "rose-pine",
+	-- 	config = function()
+	-- 		require("rose-pine").setup({
+	-- 			variant = "main", -- auto, main, moon, or dawn
+	-- 			dark_variant = "main", -- main, moon, or dawn
+	-- 			-- dim_inactive_windows = false,
+	-- 			-- extend_background_behind_borders = true,
+	-- 			-- enable = {
+	-- 			-- 	terminal = true,
+	-- 			-- 	legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+	-- 			-- 	migrations = true, -- Handle deprecated options automatically
+	-- 			-- },
+	-- 			palette = {
+	-- 				main = {
+	-- 					base = "#000000",
+	-- 					surface = "#000000",
+	-- 					overlay = "#191724",
+	-- 				},
+	-- 			},
+	-- 		})
+	--
+	-- 		vim.cmd("colorscheme rose-pine")
+	-- 		local custom_auto = require("lualine.themes.auto")
+	-- 		custom_auto.normal.c.bg = "#000000"
+	--
+	-- 		require("lualine").setup({
+	-- 			options = {
+	-- 				icons_enabled = false,
+	-- 				theme = custom_auto,
+	-- 				component_separators = "|",
+	-- 				section_separators = "",
+	-- 			},
+	-- 		})
+	-- 	end,
+	-- },
 
 	-- {
 	-- 	"ellisonleao/gruvbox.nvim",
